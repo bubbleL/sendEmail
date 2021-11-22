b534e81a7fba51cfcc4dd6d4056b5b443c48f49e
liuyuhang <1021602186@qq.com>
2021-11-22 16:30:03 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: '老公', // 收件箱展示的来件人名字
+  fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
+  user: '1021602186@qq.com', // 发送者邮箱
+  pass: 'ninilibntgxbbfac', // 发送者邮箱MTP协议密码
+  to: 'lyh2575691@outlook.com', // 发送到谁，填邮箱
+  weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
+  location: '101190101',  // 和风天气-成都武侯区的id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
+  startDay: '2018-01-13', // 在一起的日期
+}
