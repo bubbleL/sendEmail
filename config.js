9790a92cb9e33a210ef6f35f97354706e4b37970
bubbleL <50671441+bubbleL@users.noreply.github.com>
2021-11-23 10:56:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
   user: '1021602186@qq.com', // 发送者邮箱
   pass: 'ninilibntgxbbfac', // 发送者邮箱MTP协议密码
-  to: 'lyh2575691@outlook.com', // 发送到谁，填邮箱
+  to: '742001957@qq.com', // 发送到谁，填邮箱
   weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
   location: '101190101',  // 和风天气-成都武侯区的id
   type: '1,3,9', // 和风天气-生活指数type
